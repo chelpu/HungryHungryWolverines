11f556de5882c9b789d4743cbec2c16b07aa5654
Connie Qi <connieqi@umich.edu>
2014-09-06 02:42:25 -0400

    cronjob stuff

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,29 @@
 import webapp2
+# import sqlalchemy
+# import twitter
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hungry Hungry Wolverines')
 
+class SignUp(webapp2.RequestHandler):
+    def get(self):
+        import twilio.twiml
+        resp = twilio.twiml.Response()
+        resp.message('THANKS FOR SIGNING UP, d00d')
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(resp)
+
+
+class Update(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Querying for new food...')
+
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/signup', SignUp),
+    ('/update', Update),
 ], debug=True)
