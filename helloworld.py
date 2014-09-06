e8742ff1522d8f429154d76f459bdd8c538e9d77
Connie Qi <connieqi@umich.edu>
2014-09-06 00:47:29 -0400

    change helloworld text

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write('Hungry Hungry Wolverines')
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
