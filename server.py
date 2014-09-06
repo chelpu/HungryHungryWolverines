76f40fcd32ffb5c6c7a11849ad4b09ae7b97c4b4
Chelsea Pugh <chelsea.pugh93@gmail.com>
2014-09-06 14:54:09 -0400

    adding mass text capability

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import os
 import twitter
 from flask import Flask,render_template,send_from_directory,request
-import sqlalchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 # db = SQLAlchemy(app)
+
+subscribers = {}
+twilio_number = '+17348905282'
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -14,16 +16,22 @@ def main():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-	import twilio.twiml
-	resp = twilio.twiml.Response()
-	body = request.form.get('Body', 'umich free food')
-	api = twitter.Api(consumer_key=os.environ.get('CONSUMER_KEY'), consumer_secret=os.environ.get('CONSUMER_SECRET'), access_token_key=os.environ.get('ACCESS_TOK_KEY'), access_token_secret=os.environ.get('ACCESS_TOK_SECRET'))
-	result = api.GetSearch(term=body, result_type="recent", count=1)
-	resp.message(result[0].text)
-	return str(resp)
+    import twilio.twiml
+    resp = twilio.twiml.Response()
+    number = request.form.get('From', '')
+    subscribers[number] = {}
+    body = request.form.get('Body', 'umich free food')
+    api = twitter.Api(consumer_key=os.environ.get('CONSUMER_KEY'), consumer_secret=os.environ.get('CONSUMER_SECRET'), access_token_key=os.environ.get('ACCESS_TOK_KEY'), access_token_secret=os.environ.get('ACCESS_TOK_SECRET'))
+    result = api.GetSearch(term=body, result_type="recent", count=1)
+    resp.message(result[0].text)
+    return str(resp)
 
 @app.route('/update')
 def update():
+    from twilio.rest import TwilioRestClient
+    client = TwilioRestClient(account=os.environ.get('TWILIO_SID'), token=os.environ.get('TWILIO_SECRET'))
+    for number in subscribers:
+        client.sms.messages.create(body='You\'re a user', to=number, from_=twilio_number)
     return 'Querying for new food...'
 
 @app.route('/stop')
